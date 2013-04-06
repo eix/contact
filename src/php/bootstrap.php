126440799797a521fe5720b5171d2ab515b7f9b8
Max Noé <max@nohex.com>
2013-04-06 02:47:54 +0200

    Default project folders organisation updated.

--- src/php/bootstrap.php
+++ src/php/bootstrap.php
@@ -1,13 +1,24 @@
 <?php
+/**
+ * This is Eix's standard bootstrap, which adds the main/, lib/ and test/
+ * folders of the current library/phar/application into the include path, and
+ * makes sure the class loader is initialised.
+ */
 
-if (class_exists('\Nohex\\Eix\\Core\\ClassLoader')) {
-	// Classes will be loaded from the main/ folder.
-	\Nohex\Eix\Core\ClassLoader::addClassPath(__DIR__ . '/main/');
-	// If available, classes will be also loaded from the lib/ folder.
-	\Nohex\Eix\Core\ClassLoader::addClassPath(__DIR__ . '/lib/', false);
-	// If available, classes will be also loaded from the test/ folder.
-	\Nohex\Eix\Core\ClassLoader::addClassPath(__DIR__ . '/test/', false);
-} else {
-    // Eix should already be in the context.
-    throw new \RuntimeException('Eix not present.');
+// Get the class loader.
+$classLoader = null;
+if (!class_exists('\\Nohex\\Eix\\Core\\ClassLoader')) {
+	require_once __DIR__ . '/lib/nohex-eix-0.2.0.phar';
 }
+$classLoader = \Nohex\Eix\Core\ClassLoader::getInstance();
+// Add the main source folder to the include path.
+$classLoader->addPath(__DIR__ . '/main');
+// Add the libraries folder to the include path.
+$classLoader->addPath(__DIR__ . '/lib');
+// Add the test source folder to the include path.
+$classLoader->addPath(__DIR__ . '/test');
+// Initialise Eix's class loader.
+$classLoader->init();
+
+// Bring up test dependencies.
+require_once 'nohex-eix-0.2.0-devel.phar';
