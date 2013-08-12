97c63232a08e0507c996298020f594bc7aa6c427
Max Noé <max@nohex.com>
2013-08-12 19:17:32 +0200

    Dependencies are now being managed by Composer.

--- src/php/bootstrap.php
+++ src/php/bootstrap.php
@@ -1,24 +1,4 @@
 <?php
-/**
- * This is Eix's standard bootstrap, which adds the main/, lib/ and test/
- * folders of the current library/phar/application into the include path, and
- * makes sure the class loader is initialised.
- */
 
-// Get the class loader.
-$classLoader = null;
-if (!class_exists('\\Nohex\\Eix\\Core\\ClassLoader')) {
-    require_once __DIR__ . '/lib/nohex-eix-0.2.0.phar';
-}
-$classLoader = \Nohex\Eix\Core\ClassLoader::getInstance();
-// Add the main source folder to the include path.
-$classLoader->addPath(__DIR__ . '/main');
-// Add the libraries folder to the include path.
-$classLoader->addPath(__DIR__ . '/lib');
-// Add the test source folder to the include path.
-$classLoader->addPath(__DIR__ . '/test');
-// Initialise Eix's class loader.
-$classLoader->init();
-
-// Bring up test dependencies.
-require_once 'nohex-eix-0.2.0-devel.phar';
+// Composer auto-loader.
+require __DIR__ . '/lib/autoload.php';
